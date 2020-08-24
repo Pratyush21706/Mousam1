497b47dd494844f824fb1dbfefc02af3b5287cae
Pratyush21706 <65002484+Pratyush21706@users.noreply.github.com>
2020-08-24 20:45:42 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -198,7 +198,7 @@ function draw() {
         stroke("#ff3b5c");
         noFill();
         //Math.round(cases)
-        text("COMING SOON...", width / 3.8, height / 1.24)
+        text("COMING SOON...", width / 5, height / 1.24)
         fill("#ffffff")
         noStroke();
         textSize(20)
@@ -247,7 +247,7 @@ function draw() {
 
         textSize(40);
         fill("fffffff")
-        text(`loading...`, width / 2.5, height / 1.5)
+        text(`loading...`, width / 3.1, height / 1.5)
 
         textSize(20);
     }
